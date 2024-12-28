78843d14b103d4f425a3b67e86cefdfbdeabea8d
youness <youness.lerning2004@gmail.com>
2024-12-28 01:39:59 +0100

    Your commit message

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
-// https://vite.dev/config/
-export default {
-  build: {
-    outDir: 'dist', // Ensure this is set to 'dist'
-  },
-};
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+export default defineConfig({
+  plugins: [react()],
+});
